800d5c584d0a9ab285ea40127d85515b78e9689f
Tanapat <tanapat.psr@mail.kmutt.ac.th>
2022-09-11 15:22:25 +0700

    finish

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,16 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  
+  if (arrayItems == undefined || arrayItems == null){
+    return undefined ;
+
+  }else if (rowsPerPage == null || rowsPerPage == 0 || rowsPerPage == undefined){
+    return 1;
+
+  }else{
+    let x = arrayItems.length / rowsPerPage
+    return Math.ceil(x)
+}
 }
 module.exports = totalPages
